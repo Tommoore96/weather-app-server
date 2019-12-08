2c160de3bfdb0b0a1571ae3cb73b5a70b05dc0cf
Tom Moore <tom.moore@picker.me>
2019-12-08 22:09:45 +0000

    new token

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoidG9tbW9vcmU5NiIsImEiOiJjazN4azZ1N3cwMDR1M2VueHMxdG5obHg5In0.pLiWNWD5VDEinX3MLFBDsQ'
 
   request({ url, json: true }, (error, { body }) => {
     console.log('body: ', body);
